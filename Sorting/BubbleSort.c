fc92fa9aff457cf5285c65248ee62b24fab6a507
sulakshanee <sulakshanee111@gmail.com>
2021-05-04 15:56:51 +0530

    add decending order

--- Sorting/BubbleSort.c
+++ Sorting/BubbleSort.c
@@ -1,4 +1,4 @@
-// Replacing the value in the first index with the smallest value in the array and keep it repeating until the list is sorted.
+// Replacing the value in the first index with the smallest (or largest) value in the array and keep it repeating until the list is sorted.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,22 +10,24 @@ int main()
 
     printf("How many number you want to input -: ");
     scanf("%d", &count);
-    int array[count];
+    int array_1[count], array_2[count];
     printf("Enter %d integers that has to be sorted -: ", count);
     for (i = 0; i < count; i++)
     {
-        scanf("%d ", &array[i]);
+        scanf("%d ", &array_1[i]);
+        array_2[i] = array_1[i];
     }
 
+    // Ascending Order
     for (i = 0; i < count - 1; i++)
     {
         for (j = i + 1; j < count; j++)
         {
-            if (array[i] > array[j])
+            if (array_1[i] > array_1[j])
             {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                temp = array_1[i];
+                array_1[i] = array_1[j];
+                array_1[j] = temp;
             }
         }
     }
@@ -33,7 +35,27 @@ int main()
     printf("List of integers sorted in ascending order -: ");
     for (i = 0; i < count; i++)
     {
-        printf("%d ", array[i]);
+        printf("%d ", array_1[i]);
+    }
+
+    // Decending Order
+    for (i = 0; i < count - 1; i++)
+    {
+        for (j = i + 1; j < count; j++)
+        {
+            if (array_2[i] < array_2[j])
+            {
+                temp = array_2[i];
+                array_2[i] = array_2[j];
+                array_2[j] = temp;
+            }
+        }
+    }
+
+    printf("\nList of integers sorted in decending order -: ");
+    for (i = 0; i < count; i++)
+    {
+        printf("%d ", array_2[i]);
     }
 
     return 0;
